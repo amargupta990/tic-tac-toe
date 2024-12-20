975913a83e99e1bb66b1011c524a0eb2431ab03c
Amar <ag14290@gmail.com>
2024-12-21 00:30:02 +0530

    adding comments

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,24 +3,32 @@ import Card from './Card';
 import isWinner from '../helperFun';
 
 const Grid = ({ numberOfCard }) => {
+  //board is keeping the track of elemnets in the grid
   const [board, setBoard] = useState(Array(numberOfCard).fill(null));
+  //turn is keeping the track of the current player
   const [turn, setTurn] = useState(true);
+  //winner is keeping the track of the winner using  A boolean value
   const [winner, setWinner] = useState(null);
-
+//on play function is used to play the game and its does multiple task 
+// like checking the winner and updating the board and turn
   function play(index) {
+    //firstly checking if the board is already filled or winner is already declared
     if (board[index] || winner) return;
-
+//if not then updating the board and checking the winner
     const newBoard = [...board];
+    //and updating the board with the current player if true then x else o
     newBoard[index] = turn ? 'x' : 'o';
-
+//checking the winner
     const win = isWinner(newBoard, turn ? 'x' : 'o');
+    //if winner is declared then updating the winner
     if (win) {
       setWinner(turn ? 'x' : 'o');
     }
-
+//setting the board and updating the turn
     setBoard(newBoard);
     setTurn(!turn);
   }
+  //resetting the game
 
   function resetGame() {
     setBoard(Array(numberOfCard).fill(null));
@@ -40,6 +48,8 @@ const Grid = ({ numberOfCard }) => {
       <h1 className="turn-highlighter">Current turn: {turn ? 'x' : 'o'}</h1>
 
       <div className="grid">
+      //here we are mapping the board and passing the props to the card component
+      //here card component is used to display the player and on click it will call the onPlay function
         {board.map((el, index) => (
           <Card
             key={index}
